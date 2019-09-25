f8fa2b6d25adcda06ea05912e81333bbfb82fa1c
squeaky1273 <pjriddell22@gmail.com>
2019-09-25 16:59:32 -0700

    added playlists app route

--- app.py
+++ app.py
@@ -5,7 +5,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Return homepage."""
-    return render_template('home.html', msg='Flask is Cool!!')
+# OUR MOCK ARRAY OF PROJECTS
+playlists = [
+    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+]
+
+@app.route('/playlists')
+def playlists_index():
+    """Show all playlists."""
+    return render_template('playlists_index.html', playlists=playlists)
 
 
 if __name__ == '__main__':
