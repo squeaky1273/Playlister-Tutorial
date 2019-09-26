076d6ea00008e54cb983184608eff3c54eb1cb58
squeaky1273 <pjriddell22@gmail.com>
2019-09-25 20:34:01 -0700

    updated /playlists to be my root route

--- app.py
+++ app.py
@@ -2,17 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    """Return homepage."""
-# OUR MOCK ARRAY OF PROJECTS
-    playlists = [
-        { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-        { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
-    ]
-    return render_template('home.html')
 
-@app.route('/playlists')
+# OUR MOCK ARRAY OF PROJECTS
+playlists = [
+    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+]
+
+@app.route('/')
 def playlists_index():
     """Show all playlists."""
     return render_template('playlists_index.html', playlists=playlists)
