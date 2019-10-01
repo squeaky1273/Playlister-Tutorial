7155f43e8e7d274e302926672b388871c7f575d5
squeaky1273 <pjriddell22@gmail.com>
2019-09-30 17:43:18 -0700

    Comment form added

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+import os
 
 client = MongoClient()
 db = client.Playlister
+playlists = db.playlists
+
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
 playlists = db.playlists
 
 app = Flask(__name__)
@@ -57,4 +63,4 @@ def playlists_delete(playlist_id):
     return redirect(url_for('playlists_index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
